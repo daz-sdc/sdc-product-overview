3341cf1217973562dd1a8450ee307c914c48014a
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-09-29 20:56:47 -0500

    Add initial test db route

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,5 @@ const text = 'INSERT INTO users(name, email) VALUES($1, $2) RETURNING *';
 const values = ['abdiel', 'abdiel@gmail.com'];
 
 db.query(text, values)
-  .then(res => {
-    console.log(res.rows[0]);
-    // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
-  })
-  .catch(e => console.error(e.stack));
+  .then((res) => console.log(res.rows[0]))
+  .catch((e) => console.error(e.stack));
