b7b29fcba26e5d00eca147f08dbeaf0f9867d545
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-09-29 20:56:26 -0500

    Complete initial db setup

--- db/index.js
+++ db/index.js
@@ -4,5 +4,36 @@ const { Pool } = require('pg');
 const pool = new Pool();
 
 module.exports = {
-  query: (text, params) => pool.query(text, params),
+  async query(text, params) {
+    const start = Date.now();
+    const res = await pool.query(text, params);
+    const duration = Date.now() - start;
+    console.log('executed query', { text, duration, rows: res.rowCount });
+    return res;
+  },
+
+  async getClient() {
+    const client = await pool.connect();
+    const { query } = client;
+    const { release } = client;
+
+    const timeout = setTimeout(() => {
+      console.error('A client has been checked out for more than 5 seconds!');
+      console.error(`The last executed query on this client was: ${client.lastQuery}`);
+    }, 5000);
+
+    client.query = (...args) => {
+      client.lastQuery = args;
+      return query.apply(client, args);
+    };
+
+    client.release = () => {
+      clearTimeout(timeout);
+      client.query = query;
+      client.release = release;
+      return release.apply(client);
+    };
+    return client;
+  },
+
 };
