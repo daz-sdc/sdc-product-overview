5489d68842137d24519dd547339071786109fa24
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-10-04 08:34:54 -0500

    Add initial database queries

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,8 @@
-module.exports = (req, res) => {
-  const count = req.query.count || 5;
+const models = require('../models/index');
+
+module.exports = async (req, res) => {
   const page = req.query.page || 1;
-  res.status(200).send(`Products! Page: ${page}, Count: ${count}`);
+  const count = req.query.count || 5;
+  const data = await models.products(page, count);
+  res.status(200).send(data.rows);
 };
--- models/products.js
+++ models/products.js
@@ -1,3 +1,12 @@
+const db = require('../db/index');
+
 module.exports = (page, count) => {
-  console.log(page, count);
+  const text = `
+  SELECT * FROM products
+  ORDER BY product_id
+  LIMIT ${count}
+  OFFSET (${page} - 1) * 5
+  `;
+
+  return db.query(text);
 };
