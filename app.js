b9ff3ca729c851cace21895640e1ff55f702844d
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-10-07 09:04:21 -0500

    Add route handler for root path /

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const router = require('./routes');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.status(200).send('Product Overview SDC');
+});
+
 app.use(router);
 
 module.exports = app;
