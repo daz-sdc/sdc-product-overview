70db6b5bd4f8bcb6ba0d0b59985cb337f6a5686a
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-10-08 13:32:22 -0500

    Initialize styles controller

--- controllers/styles.js
+++ controllers/styles.js
@@ -1,4 +1,9 @@
-module.exports = (req, res) => {
+const models = require('../models/index');
+
+module.exports = async (req, res) => {
   const id = req.params.product_id;
-  res.status(200).send(`Styles for product id: ${id}!`);
+
+  const styles = await models.getStyles(id);
+
+  res.status(200).send(styles.rows);
 };
