6f90d7abfd70ea59f808157af6b22ee974de5eb3
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-10-11 09:43:13 -0500

    Fix column name

--- models/getRelated.js
+++ models/getRelated.js
@@ -1,7 +1,7 @@
 const db = require('../db/index');
 
 module.exports = (id) => {
-  const text = 'SELECT related_id FROM related_products WHERE product_id = $1';
+  const text = 'SELECT related_product_id FROM related_products WHERE product_id = $1';
   const params = [id];
 
   return db.query(text, params);
