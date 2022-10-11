96f580061e83a6d81a5a484ef684a94dd90cc366
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-10-11 10:02:58 -0500

    Complete initial getRelated controller and model

--- controllers/related.js
+++ controllers/related.js
@@ -1,4 +1,8 @@
-module.exports = (req, res) => {
+const models = require('../models/index');
+
+module.exports = async (req, res) => {
   const id = req.params.product_id;
-  res.status(200).send(`Related products for product id: ${id}!`);
+  const related = await models.getRelated(id);
+
+  res.status(200).send(related.rows[0].array);
 };
