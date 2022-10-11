fbac28ef396d19d369fc590ec81ae1f60d43ae98
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-10-11 09:33:32 -0500

    Complete models for styles controller

--- models/getPhotos.js
+++ models/getPhotos.js
@@ -1,7 +1,7 @@
 const db = require('../db/index');
 
 module.exports = (id) => {
-  const text = 'SELECT * FROM styles_photos WHERE style_id = $1';
+  const text = 'SELECT thumbnail_url, url FROM styles_photos WHERE style_id = $1';
   const params = [id];
 
   return db.query(text, params);
--- models/getSkus.js
+++ models/getSkus.js
@@ -1,7 +1,7 @@
 const db = require('../db/index');
 
 module.exports = (id) => {
-  const text = 'SELECT * FROM styles_skus WHERE style_id = $1';
+  const text = 'SELECT sku_id, quantity, size FROM styles_skus WHERE style_id = $1';
   const params = [id];
 
   return db.query(text, params);
