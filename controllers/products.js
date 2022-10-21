31de58b5063e9580690721e28e46e4761993adb6
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-10-20 21:17:36 -0500

    Add test for each route

--- controllers/products.js
+++ controllers/products.js
@@ -4,10 +4,10 @@ module.exports = async (req, res) => {
   const count = Number(req.query.count) || 5;
   const page = Number(req.query.page) || 1;
 
-  if (typeof count !== 'number' || typeof page !== 'number') {
-    res.status(400).end();
-    return;
-  }
+  // if (typeof count !== 'number' || typeof page !== 'number') {
+  //   res.status(400).end();
+  //   return;
+  // }
 
   const data = await models.getProducts(count, page);
   res.status(200).send(data.rows);
--- tests/server.test.js
+++ tests/server.test.js
@@ -37,4 +37,46 @@ describe('/products', () => {
     });
   });
 
+
+
+});
+
+describe('/products/:product_id', () => {
+  test('It should return the product features in the correct format', async () => {
+    const response = await request(app).get('/products/12345');
+    expect(response.body.features.length).toBe(2);
+    expect(response.body.features[0].feature).toEqual('5 Year Warranty');
+    expect(response.body.features[0].value).toEqual('null');
+    expect(response.body.features[1].feature).toEqual('Frame');
+    expect(response.body.features[1].value).toEqual('DuraResin');
+
+  });
+});
+
+
+describe('/products/:product_id/styles', () => {
+  test('It should return the product styles in the correct format', async () => {
+    const response = await request(app).get('/products/12345/styles');
+    let styles = response.body.results;
+    expect(response.body.product_id).toBe('12345');
+    expect(styles[0].style_id).toBe(24354);
+    expect(styles[0].photos.length).toBe(4);
+    expect(Object.keys(styles[0].skus).length).toBe(6);
+
+    expect(styles[1].style_id).toBe(24353);
+    expect(styles[1].photos.length).toBe(3);
+    expect(Object.keys(styles[1].skus).length).toBe(6);
+
+    expect(styles[2].style_id).toBe(24352);
+    expect(styles[2].photos.length).toBe(3);
+    expect(Object.keys(styles[2].skus).length).toBe(6);
+  });
+});
+
+describe('/products/:product_id/related', () => {
+  test('It should return an array of the related product IDs', async () => {
+    const response = await request(app).get('/products/12345/related');
+    expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body.length).toBe(4);
+  });
 });
